ab8f862cb45be4966ef0435140d1d903061ea477
Tomek Skuta <kontakt@tomekskuta.pl>
2025-03-22 13:23:21 +0100

    feat: configure plugins for better development experience
    - Enable Avante with Copilot integration and Claude model
    
    - Improve treesitter configuration with ejs support
    
    - Enhance comment plugin with context-aware commenting
    
    - Remove redundant CopilotChat configuration
    
    Signed-off-by: Tomek Skuta <kontakt@tomekskuta.pl>

--- lua/plugins/comment-code.lua
+++ lua/plugins/comment-code.lua
@@ -1,10 +1,20 @@
 return {
 	"numToStr/Comment.nvim",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
 	opts = {
 		-- add any options here
 	},
 	lazy = false,
-	config = function()
-		require("Comment").setup()
-	end,
+config = function()
+	require("ts_context_commentstring").setup({
+		enable_autocmd = false, -- Important: disabled here as we're setting via Comment.nvim
+	})
+
+	require("Comment").setup({
+		pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		ignore = "^$", -- Allow commenting empty lines
+	})
+end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local TSconfig = require("nvim-treesitter.configs")
-    TSconfig.setup({
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      languages = {
-        ejs = "html",
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local TSconfig = require("nvim-treesitter.configs")
+		TSconfig.setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			languages = {
+				ejs = "html",
+			},
+			ensure_installed = { "html", "javascript", "typescript", "tsx", "embedded_template" },
+		})
+
+		vim.filetype.add({
+			extension = {
+				ejs = "ejs",
+			},
+		})
+		vim.treesitter.language.register("html", "ejs")
+		vim.treesitter.language.register("javascript", "ejs")
+		vim.treesitter.language.register("embedded_template", "ejs")
+	end,
 }
