30365e5804efcefa7958cd9ea7b52e0bdbd46707
Tomek Skuta <kontakt@tomekskuta.pl>
2025-11-02 12:43:06 +0100

    chore: update plugin lockfile, simplify Avante config and disable none-ls format-on-save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -12,16 +12,16 @@ return {
 			},
 			on_attach = function(client, bufnr)
 				-- Enable formatting on save
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
+				-- if client.supports_method("textDocument/formatting") then
+				-- 	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+				-- 		group = augroup,
+				-- 		buffer = bufnr,
+				-- 		callback = function()
+				-- 			vim.lsp.buf.format({ bufnr = bufnr })
+				-- 		end,
+				-- 	})
+				-- end
 			end,
 		})
 
